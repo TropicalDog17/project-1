48d8ec86b27fb20822efd424801c949aaf63c5a4
Tuan Tran <tdtuan1702@gmail.com>
2022-08-17 14:22:01 +0700

    Simplified code

--- bt1/utils/saving_article.py
+++ bt1/utils/saving_article.py
@@ -1,6 +1,6 @@
 
 def saving_article(collection, link, title, content,comments=[]):
-    """Luu cac thong tin bai bao vao database
+    """Luu cac thong tin bai bao vao database, neu da co du lieu san trung lap thi khong them vao
     
     Args: 
         collection: se luu cac bai bao vao class nay
@@ -9,11 +9,7 @@ def saving_article(collection, link, title, content,comments=[]):
         content: Noi dung duoi dang text
         comments(optional): Danh sach tat ca cac comment(List)
     """
-    if not collection.objects(title=title):
-        article = collection(link=link, title=title, content=content, comments=comments).save()
-    elif not collection.objects(content=content):
-        article = collection(link=link, title=title, content=content, comments=comments).save()
-    elif not collection.objects(comments=comments):
+    if not collection.objects(title=title) or not collection.objects(content=content) or not collection.objects(comments=comments): 
         article = collection(link=link, title=title, content=content, comments=comments).save()
     else:
         return
