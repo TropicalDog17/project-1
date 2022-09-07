1492922998a9c53e266e777feb04556a0208691e
Tuan Tran <tdtuan1702@gmail.com>
2022-09-07 15:52:30 +0700

    feat: Finish bt3

--- bt3/main.py
+++ bt3/main.py
@@ -1,3 +1,13 @@
-from tasks import scraping_vnexpress
+from tasks import get_article_links, scrape_page
+SCRAPE_URL_LIST = (
+    'https://vnexpress.net/tin-tuc/giao-duc-p' + str(i) for i in range(1, 20))
+SCRAPE_URL = 'https://vnexpress.net/tin-tuc/giao-duc'
+def scraping_vnexpress():
+    try:
+        for i in SCRAPE_URL_LIST:
+            (get_article_links.s(i) | scrape_page.s()).apply_async()
+    except Exception as e:
+        print('The scraping job failed. See exception: ')
+        print(e)
 if __name__ == "__main__":
-    scraping_vnexpress.delay()
+    scraping_vnexpress()
--- bt3/tasks.py
+++ bt3/tasks.py
@@ -1,56 +1,46 @@
 import re
 
-from celery import Celery
+from celery import Celery, group
 import requests  # pulling data
 from bs4 import BeautifulSoup  # xml parsing
 import json  # exporting to files
 import redis
 import uuid
+import json
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
-app.conf.result_backend = 'redis://localhost:6379/0'
-SCRAPE_URL_LIST = (
-    'https://vnexpress.net/tin-tuc/giao-duc-p' + str(i) for i in range(1, 20))
-SCRAPE_URL = 'https://vnexpress.net/tin-tuc/giao-duc'
+app = Celery('tasks', broker='pyamqp://guest@localhost//', backend='redis://localhost:6379/1')
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 pipe = r.pipeline()
-
-
 def remove_duplicate_link(links):
     return list(set(links))
 
 
-def get_article_links(soup):
-    return (item["href"] for item in soup.find_all("a", attrs={"href": re.compile("^https://vnexpress.net")}))
-
-@app.task
+@app.task()
 def scrape_article(article_link):
     article_r = requests.get(article_link)
     article_id = uuid.uuid4()
     article_soup = BeautifulSoup(article_r.content, "lxml")
     article_title = article_soup.find("h1", {"class": "title-detail"}).get_text()
     article_content = article_soup.find("article", {"class": "fck_detail"}).get_text()
-
     # from each "item"
     pipe.hset(str(article_id), "link", article_link)
-    pipe.hset(str(article_id), "title", article_title)
+    pipe.hset(str(article_id),"title", article_title)
     pipe.hset(str(article_id), "content", article_content)
+    pipe.execute()
 
-@app.task
-def scrape_page(link):
-    req = requests.get(link)
+
+@app.task(ignore_result=True)
+def get_article_links(page_link):
+    result = []
+    req = requests.get(page_link)
     soup = BeautifulSoup(req.content, "lxml")
-    article_links = get_article_links(soup)
-    for article_link in article_links:
-        scrape_article.delay(article_link)
-@app.task
-def scraping_vnexpress():
-    article_list = []
-    try:
-        for link in SCRAPE_URL_LIST:
-            scrape_page.delay(link)
-        pipe.execute()
-    except Exception as e:
-        print('The scraping job failed. See exception: ')
-        print(e)
+    for item in soup.find_all("a", attrs={"href": re.compile("^https://vnexpress.net")}):
+        result.append(item["href"])
+    return result
+
+
+@app.task(ignore_result=True)
+def scrape_page(article_links):
+    for i in article_links:
+        scrape_article.delay(i)
