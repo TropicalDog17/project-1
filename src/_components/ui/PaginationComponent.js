86fd6c5e5882cc2a853dfef6dbf2d624727bf28f
Tuan Tran <tdtuan1702@gmail.com>
2022-09-22 15:27:21 +0700

    refactor: Change Pagination UI

--- src/_components/ui/PaginationComponent.js
+++ src/_components/ui/PaginationComponent.js
@@ -2,27 +2,27 @@ import Pagination from "react-bootstrap/Pagination"; //Duplicated name
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { articleAtom, currentPageSelector, pageIndexAtom } from "../../state";
 import { PaginationItemList } from "./PaginationItemList";
+import { MAX_ARTICLES_PER_PAGE } from "../../common";
 export { PaginationComponent };
 function PaginationComponent() {
   const handleNext = useHandleNext();
   const handlePrev = useHandlePrev();
-  const handlePageIndex = useHandlePageIndex();
+  const handleFirst = useHandleFirst();
+  const handleLast = useHandleLast();
   return (
-    <Pagination>
-      <Pagination.First />
-      <Pagination.Prev onClick={handlePrev} />
+    <Pagination size="lg">
+      <Pagination.First onClick={handleFirst}>First</Pagination.First>
+      <Pagination.Prev onClick={handlePrev} className="me-2">
+        Previous
+      </Pagination.Prev>
       <PaginationItemList />
-      <Pagination.Next onClick={handleNext} />
-      <Pagination.Last />
+      <Pagination.Next onClick={handleNext} className="ms-2">
+        Next
+      </Pagination.Next>
+      <Pagination.Last onClick={handleLast}>Last</Pagination.Last>
     </Pagination>
   );
-  function useHandlePageIndex() {
-    const setPageIndex = useSetRecoilState(pageIndexAtom);
-    function handlePageIndex(index) {
-      setPageIndex(index);
-    }
-    return handlePageIndex;
-  }
+
   function useHandlePrev() {
     const [pageIndex, setPageIndex] = useRecoilState(pageIndexAtom);
     const articles = useRecoilState(articleAtom);
@@ -42,7 +42,6 @@ function PaginationComponent() {
     const articleList = useRecoilValue(currentPageSelector);
     const articles = useRecoilValue(articleAtom);
 
-    const MAX_ARTICLES_PER_PAGE = 5;
     function handleNext() {
       if (articles.length / MAX_ARTICLES_PER_PAGE < pageIndex) {
         setPageIndex(pageIndex);
@@ -51,5 +50,23 @@ function PaginationComponent() {
       }
     }
     return handleNext;
+  }
+  function useHandleFirst() {
+    const [pageIndex, setPageIndex] = useRecoilState(pageIndexAtom);
+    function handleFirst() {
+      setPageIndex(1);
+    }
+    return handleFirst;
+  }
+  function useHandleLast() {
+    const [pageIndex, setPageIndex] = useRecoilState(pageIndexAtom);
+    const articles = useRecoilValue(articleAtom);
+
+    function handleLast() {
+      if (!articles) return;
+      console.log(articles.length);
+      setPageIndex(Math.ceil(articles.length / MAX_ARTICLES_PER_PAGE));
+    }
+    return handleLast;
   }
 }
--- src/_components/ui/PaginationItemList.js
+++ src/_components/ui/PaginationItemList.js
@@ -9,7 +9,7 @@ function PaginationItemList() {
   const articles = useRecoilValue(articleAtom);
   const length = Math.ceil(articles.length / MAX_ARTICLES_PER_PAGE);
   const handlePageIndex = useHandlePageIndex();
-
+  const pageIndex = useRecoilValue(pageIndexAtom);
   if (!articles || length < 0) return;
   else if (length === 1)
     return (
@@ -19,26 +19,48 @@ function PaginationItemList() {
     return (
       <Fragment>
         {Array.from(Array(length).keys()).map((_, idx) => {
-          <Pagination.Item onClick={() => handlePageIndex(idx + 1)}>
+          <Pagination.Item
+            onClick={() => handlePageIndex(idx + 1)}
+            key={idx + 1}
+            active={idx + 1 === pageIndex}
+            className="w-100"
+          >
             {idx + 1}
           </Pagination.Item>;
         })}
       </Fragment>
     );
-  const middlePageIndex = Math.floor(length / 2);
-  return (
-    <Fragment>
-      <Pagination.Item onClick={() => handlePageIndex(1)}>{1}</Pagination.Item>
-      <Pagination.Ellipsis />
-      <Pagination.Item onClick={() => handlePageIndex(middlePageIndex)}>
-        {middlePageIndex}
-      </Pagination.Item>
-      <Pagination.Ellipsis />
-      <Pagination.Item onClick={() => handlePageIndex(length)}>
-        {length}
-      </Pagination.Item>
-    </Fragment>
-  );
+  // const middlePageIndex = Math.floor(length / 2);
+  if (pageIndex < 4) {
+    return (
+      <Fragment>
+        {Array.from(Array(4).keys()).map((_, idx) => (
+          <Pagination.Item
+            onClick={() => handlePageIndex(idx + 1)}
+            key={idx + 1}
+            active={idx + 1 === pageIndex}
+          >
+            {idx + 1}
+          </Pagination.Item>
+        ))}
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        {Array.from(Array(4).keys()).map((_, idx) => (
+          <Pagination.Item
+            onClick={() => handlePageIndex(idx + pageIndex - 3)}
+            key={idx + pageIndex - 3}
+            active={idx + pageIndex - 3 === pageIndex}
+          >
+            {idx + pageIndex - 3}
+          </Pagination.Item>
+        ))}
+      </Fragment>
+    );
+  }
+
   function useHandlePageIndex() {
     const setPageIndex = useSetRecoilState(pageIndexAtom);
     function handlePageIndex(index) {
